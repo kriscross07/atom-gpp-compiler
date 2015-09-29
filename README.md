6a184b82f338d3f5c47edbe7736cd3fdfe6024ea
Kris <kriscross07@gmail.com>
2015-09-29 00:10:11 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# gpp-compiler package
+
+This package allows you to press f5 to compile and run the selected .cpp file with g++.
+
+You could probably configure this package to work with linux/mac, but currently there is no support for linux/mac.
+
+## What you need
+- g++ (you can install g++ by installing MinGW from [mingw.org](http://www.mingw.org/))
+- "C:\MinGW\bin\" added to path.
---
+++ index.js
@@ -0,0 +1,34 @@
+var exec=require('child_process').exec,fs=require('fs');
+
+module.exports.activate=function(){
+  atom.commands.add('atom-text-editor','gpp-compiler:compile',compile);
+  atom.commands.add('.tree-view .file>.name','gpp-compiler:treeCompile',treeCompile);
+};
+
+function compile(treePath){
+  var editor=atom.workspace.getActiveTextEditor();
+  if(!editor)return;
+  editor.save();
+  var path,dir,file,ext;
+  path=typeof treePath=='string'?treePath:editor.getPath();
+  dir=path.split('\\');
+  dir.pop();
+  dir=dir.join('\\');
+  file=path.split('\\').pop().split('.');
+  file.pop();
+  file=file.join('.');
+  ext='.'+path.split('\\').pop().split('.').pop();
+  console.log(path,dir,file,ext);
+  exec('g++ '+file+ext+' -o '+file,{cwd:dir},function(err,stdout,stderr){
+    if(stderr){
+      fs.writeFile(dir+'compiling_error.txt',stderr);
+      atom.notifications.add('error',stderr.replace(/\n/g,'<br>'));
+    }
+    else exec('start '+file,{cwd:dir});
+  });
+}
+function treeCompile(e){
+  var path=e.target.getAttribute('data-path');
+  console.log(path);
+  compile(path);
+}
