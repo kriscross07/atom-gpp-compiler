15afa5167ee119e957074fbbb030735730df6023
Kris <kriscross07@gmail.com>
2015-10-05 19:25:58 -0400

    Now uses path.parse()

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var exec = require("child_process").exec, fs = require("fs");
+var exec = require("child_process").exec, fs = require("fs"), parse = require("path").parse;
 
 module.exports = {
-  activate: function() {
+  activate: function(){
     atom.commands.add("atom-text-editor", "gpp-compiler:compile", compile);
-    atom.commands.add(".tree-view .file>.name", "gpp-compiler:treeCompile", treeCompile);
+    atom.commands.add(".tree-view .file > .name", "gpp-compiler:treeCompile", treeCompile);
   },
   config: {
     addCompilingErr: {
@@ -15,32 +15,22 @@ module.exports = {
   }
 };
 
-function compile(treePath) {
+function compile(treePath){
   var editor = atom.workspace.getActiveTextEditor();
-  if(!editor)return;
-  editor.save();
-  var path, dir, file, ext;
-  path = typeof(treePath) == "string" ? treePath : editor.getPath();
-  dir = path.split("\\");
-  dir.pop();
-  dir = dir.join("\\") + "\\";
-  file = path.split("\\").pop().split(".");
-  file.pop();
-  file = file.join(".");
-  ext = "." + path.split("\\").pop().split(".").pop();
-  exec("g++ " + file + ext + " -o " + file,{cwd: dir},function(err,stdout,stderr) {
-    if(stderr) {
-      atom.notifications.add("error", stderr.replace(/\n/g,"<br>"));
-      if(atom.config.get("gpp-compiler.addCompilingErr"))fs.writeFile(dir+"compiling_error.txt",stderr);
+  var info = parse(typeof(treePath) == "string" ? treePath : editor.getPath());
+  exec("g++ " + info.base + " -o " + info.name, {cwd: info.dir}, function(err, stdout, stderr){
+    if(stderr){
+      atom.notifications.add("error", stderr.replace(/\n/g, "<br>"));
+      if(atom.config.get("gpp-compiler.addCompilingErr"))fs.writeFile(info.dir + "compiling_error.txt", stderr);
     }
-    else {
-      exec("start " + file, {cwd:dir});
-      fs.readFile(dir + "compiling_error.txt", function(err) {
+    else{
+      exec("start " + info.name, {cwd: info.dir});
+      fs.readFile(info.dir + "compiling_error.txt", function(err){
         if(!err)fs.unlink(dir + "compiling_error.txt");
       });
     }
   });
 }
-function treeCompile(e) {
+function treeCompile(e){
   compile(e.target.getAttribute("data-path"));
 }
