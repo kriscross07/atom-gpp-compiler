0571704d7d5f000e1587cf5613c6154dca8622f2
kriscross07 <kriscross07@gmail.com>
2015-12-02 17:29:37 -0500

    Added support for linux and possibly mac

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var exec = require("child_process").exec, fs = require("fs"), parse = require("path").parse;
+/* jshint node: true */
+/* globals atom */
+"use strict";
+var exec = require("child_process").exec;
+var fs = require("fs");
+var parse = require("path").parse;
 
 module.exports = {
-  activate: function(){
+  activate: function() {
     atom.commands.add("atom-text-editor", "gpp-compiler:compile", compile);
     atom.commands.add(".tree-view .file > .name", "gpp-compiler:treeCompile", treeCompile);
   },
@@ -15,23 +20,39 @@ module.exports = {
   }
 };
 
-function compile(treePath){
+function compile(treePath) {
   var editor = atom.workspace.getActiveTextEditor();
-  if(editor)editor.save();
-  var info = parse(typeof(treePath) == "string" ? treePath : editor.getPath());
-  exec("g++ \"" + info.base + "\" -o \"" + info.name + "\"", {cwd: info.dir}, function(err, stdout, stderr){
-    if(stderr){
-      atom.notifications.add("error", stderr.replace(/\n/g, "<br>"));
-      if(atom.config.get("gpp-compiler.addCompilingErr"))fs.writeFile(info.dir + "/compiling_error.txt", stderr);
-    }
-    else{
-      exec("start \"" + info.name + "\" \"" + info.name + "\"", {cwd: info.dir});
-      fs.readFile(info.dir + "/compiling_error.txt", function(err){
-        if(!err)fs.unlink(info.dir + "/compiling_error.txt");
+  if (editor) {
+    editor.save();
+  }
+  var info = parse(typeof treePath == "string" ? treePath : editor.getPath());
+  exec("g++ \"" + info.base + "\" -o \"" + info.name + "\"", {cwd: info.dir}, function(err, stdout, stderr) {
+    if (stderr) {
+      if (atom.notifications) {
+        atom.notifications.add("error", stderr.replace(/\n/g, "<br>"));
+      }
+      if (atom.config.get("gpp-compiler.addCompilingErr")) {
+        fs.writeFile(info.dir + "/compiling_error.txt", stderr);
+      }
+    } else {
+      if (process.platform == "win32") {
+        exec("start \"" + info.name + "\" \"" + info.name + "\"", {
+          cwd: info.dir
+        });
+      } else if (process.platform == "linux") {
+        exec("gnome-terminal -e \"./" + info.name.replace(/ /g, "\\ ") + "\"", {cwd: info.dir});
+      } else if (process.platform == "darwin") {
+        exec("open \"" + info.name.replace(/ /g, "\\ ") + "\"");
+      }
+      fs.readFile(info.dir + "/compiling_error.txt", function(err) {
+        if (!err) {
+          fs.unlink(info.dir + "/compiling_error.txt");
+        }
       });
     }
   });
 }
-function treeCompile(e){
+
+function treeCompile(e) {
   compile(e.target.getAttribute("data-path"));
 }
