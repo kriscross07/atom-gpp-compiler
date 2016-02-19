c944d878fbd99844d960741b8bca9e55f65db668
kriscross07 <kriscross07@gmail.com>
2016-02-18 19:58:49 -0500

    Recoded, allowing compilation of multiple files.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-/* jshint node: true */
+/* jshint node: true, browser: true, esnext: true */
 /* globals atom */
 "use strict";
-var exec = require("child_process").exec;
-var esc = require("shell-escape");
+var child_process = require("child_process");
 var fs = require("fs");
-var parse = require("path").parse;
+var path = require("path");
 
 module.exports = {
-  activate: function() {
-    atom.commands.add("atom-text-editor", "gpp-compiler:compile", compile);
-    atom.commands.add(".tree-view .file > .name", "gpp-compiler:treeCompile", treeCompile);
+  activate: () => {
+    atom.commands.add("atom-text-editor", "gpp-compiler:f5Compile", f5Compile);
+    atom.commands.add(".tree-view .file", "gpp-compiler:treeCompile", treeCompile);
   },
   config: {
     addCompilingErr: {
@@ -39,55 +38,83 @@ module.exports = {
   }
 };
 
-function compile(treePath) {
+function compile(e, files, info) {
   var editor = atom.workspace.getActiveTextEditor();
+	var args = [];
+	var i;
   if (editor) {
     editor.save();
   }
-  var info = parse(typeof treePath == "string" ? treePath : editor.getPath());
-  if (atom.config.get("gpp-compiler.fileExtension")) {
-    info.name += "." + atom.config.get("gpp-compiler.fileExtension");
-  }
-  if (process.platform == "win32") {
-    exec("g++ \"" + info.base + "\" -o \"" + info.name + "\" " + atom.config.get("gpp-compiler.gppOptions"), {cwd: info.dir}, function(err, stdout, stderr) {
-      onCompile(err, stdout, stderr, info);
-    });
-  } else {
-    exec(esc(["g++", info.base, "-o", info.name]) + " " + atom.config.get("gpp-compiler.gppOptions"), {cwd: info.dir}, function(err, stdout, stderr) {
-      onCompile(err, stdout, stderr, info);
-    });
-  }
-}
-
-function onCompile(err, stdout, stderr, info) {
-  if (stderr) {
-    atom.notifications.add("error", stderr.replace(/\n/g, "<br>"));
-    if (atom.config.get("gpp-compiler.addCompilingErr")) {
-      fs.writeFile(info.dir + "/compiling_error.txt", stderr);
-    }
-  } else {
-    if (atom.config.get("gpp-compiler.runAfterCompile")) {
-      if (process.platform == "win32") {
-        exec("start \"" + info.name + "\" \"" + info.name + "\"", {
-          cwd: info.dir
-        });
-      } else if (process.platform == "linux") {
-        exec(esc(["xterm", "-hold", "-e", "./" + info.name]), {cwd: info.dir});
-      } else if (process.platform == "darwin") {
-        exec("open \"" + info.name.replace(/ /g, "\\ ") + "\"");
-      }
+	if (atom.config.get("gpp-compiler.fileExtension")) {
+		info.name += "." + atom.config.get("gpp-compiler.fileExtension");
+	}
+	for (i in files) {
+		args.push(files[i]);
+	}
+	args.push("-o");
+	args.push(info.name);
+	var userArgs = atom.config.get("gpp-compiler.gppOptions").split(" ");
+	for (i in userArgs) {
+		if (userArgs[i]) {
+			args.push(userArgs[i]);
+		}
+	}
+	var child = child_process.spawn("g++", args, {
+		cwd: info.dir
+	});
+	var stderr = "";
+	child.stderr.on("data", (data) => {
+		stderr += data;
+	});
+	child.on("close", (code) => {
+		if (code) {
+			atom.notifications.add("error", stderr.replace(/\n/, ""));
+			if (atom.config.get("gpp-compiler.addCompilingErr")) {
+				fs.writeFile(info.dir + "/compiling_error.txt", stderr);
+			}
+		} else {
+			if (atom.config.get("gpp-compiler.runAfterCompile")) {
+				if (process.platform == "win32") {
+					child_process.spawn("start", [info.name, info.name], {
+						cwd: info.dir
+					});
+				} else if (process.platform == "linux") {
+					child_process.spawn("xterm", ["-hold", "-e", "./" + info.name], {
+						cwd: info.dir
+					});
+				} else if (process.platform == "darwin") {
+					child_process.spawn("open", info.name, {
+						cwd: info.dir
+					});
+				}
+			} else {
+				atom.notifications.add("success", "Compiling successful");
+			}
       fs.readFile(info.dir + "/compiling_error.txt", function(err) {
         if (!err) {
           fs.unlink(info.dir + "/compiling_error.txt");
         }
       });
-    }
-    else {
-      atom.notifications.addSuccess("Compiling succesful");
-    }
-  }
+		}
+	});
 }
 
 function treeCompile(e) {
-  compile(e.target.getAttribute("data-path"));
+	var names = document.querySelectorAll(".tree-view .file.selected > .name");
+	var files = [];
+	var element = e.target;
+	if (element.classList.contains("file")) {
+		element = element.firstChild;
+	}
+	for (var i in names) {
+		if (names[i] instanceof HTMLElement) {
+			files.push(names[i].getAttribute("data-path"));
+		}
+	}
+	compile(null, files, path.parse(element.getAttribute("data-path")));
+}
+
+function f5Compile() {
+	var file = atom.workspace.getActiveTextEditor().buffer.file.path;
+	compile(null, [file], path.parse(file));
 }
