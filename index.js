7036f843bd66cf18e6e32ab29384bd17d500c0a1
kriscross07 <kriscross07@gmail.com>
2016-04-15 11:55:34 -0400

    added support for changing linux terminal limited line length to eighty characters

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-/* jshint node: true, browser: true, esnext: true */
+/* jshint node: true, browser: true */
 /* globals atom */
 "use strict";
+
 var child_process = require("child_process");
 var fs = require("fs");
 var path = require("path");
@@ -8,53 +9,66 @@ var path = require("path");
 module.exports = {
   activate: () => {
     atom.commands.add("atom-text-editor", "gpp-compiler:f5Compile", f5Compile);
-    atom.commands.add(".tree-view .file", "gpp-compiler:treeCompile", treeCompile);
+    atom.commands.add(
+      ".tree-view .file",
+      "gpp-compiler:treeCompile",
+      treeCompile
+    );
   },
   config: {
     addCompilingErr: {
+      title: "Add compiling_error.txt",
       type: "boolean",
       default: true,
-      title: "Add compiling_error.txt",
-      description: "Add a file named \"compiling_error.txt\" if compiling goes wrong."
+      description: "Add a file named \"compiling_error.txt\" if compiling" +
+                   " goes wrong"
     },
     runAfterCompile: {
+      title: "Run after compile",
       type: "boolean",
       default: true,
-      title: "Run after compile",
       description: "Run program after compiling is done"
     },
     fileExtension: {
+      title: "File extension",
       type: "string",
       default: "",
-      title: "File extension",
       description: "Extension of compiled file"
     },
     gppOptions: {
+      title: "g++ options",
       type: "string",
       default: "",
-      title: "g++ options",
       description: "g++ command line options"
     }
   }
 };
 
+if (process.platform == "linux") {
+  module.exports.config.linuxTerminal = {
+    title: "Linux terminal",
+    type: "string",
+    default: "XTerm",
+    enum: ["XTerm", "GNOME Terminal", "Konsole", "xfce4-terminal"]
+  };
+}
+
 function compile(e, files, info) {
   var editor = atom.workspace.getActiveTextEditor();
   var args = [];
-  var i;
   if (editor) {
     editor.save();
   }
   if (atom.config.get("gpp-compiler.fileExtension")) {
     info.name += "." + atom.config.get("gpp-compiler.fileExtension");
   }
-  for (i in files) {
+  for (let i in files) {
     args.push(files[i]);
   }
   args.push("-o");
   args.push(info.name);
   var userArgs = atom.config.get("gpp-compiler.gppOptions").split(" ");
-  for (i in userArgs) {
+  for (let i in userArgs) {
     if (userArgs[i]) {
       args.push(userArgs[i]);
     }
@@ -70,18 +84,41 @@ function compile(e, files, info) {
     if (code) {
       atom.notifications.add("error", stderr.replace(/\n/, ""));
       if (atom.config.get("gpp-compiler.addCompilingErr")) {
-        fs.writeFile(info.dir + "/compiling_error.txt", stderr);
+        fs.writeFile(path.join(info.dir, "compile_error.txt"), stderr);
       }
     } else {
       if (atom.config.get("gpp-compiler.runAfterCompile")) {
         if (process.platform == "win32") {
-          child_process.exec("start " + escapeArg(info.name) + " " + escapeArg(info.name), {
+          child_process.exec("start " +
+                             escapeArg(info.name) +
+                             " " +
+                             escapeArg(info.name), {
             cwd: info.dir
           });
         } else if (process.platform == "linux") {
-          child_process.spawn("xterm", ["-hold", "-e", "./" + info.name], {
-            cwd: info.dir
-          });
+          var terminal = atom.config.get("gpp-compiler.linuxTerminal");
+          if (terminal == "GNOME Terminal") {
+            child_process.spawn("gnome-terminal", ["--command", path.join(
+              info.dir,
+              info.name
+            )]);
+          } else if (terminal == "Konsole") {
+            child_process.spawn("konsole", ["--hold", "-e", path.join(
+              info.dir,
+              info.name
+            )]);
+          } else if (terminal == "xfce4-terminal") {
+            child_process.spawn("xfce4-terminal", [
+              "--hold",
+              "--command",
+              path.join(info.dir, info.name),
+            ]);
+          } else {
+            child_process.spawn("xterm", ["-hold", "-e", path.join(
+              info.dir,
+              info.name
+            )]);
+          }
         } else if (process.platform == "darwin") {
           child_process.spawn("open", [info.name], {
             cwd: info.dir
@@ -106,7 +143,7 @@ function treeCompile(e) {
   if (element.classList.contains("file")) {
     element = element.firstChild;
   }
-  for (var i in names) {
+  for (let i in names) {
     if (names[i] instanceof HTMLElement) {
       files.push(names[i].getAttribute("data-path"));
     }
