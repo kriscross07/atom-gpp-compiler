80ea189016423f32f1bb062de404c94527aaca13
kriscross07 <kriscross07@gmail.com>
2016-07-06 20:45:07 -0400

    somehow git reset my commits back to 1, but i loaded a backup and ran `git push` to revert back to how it was. this commit puts it like it was, while preserving the commits

--- README.md
+++ README.md
@@ -1,17 +1,58 @@
 # gpp-compiler
 
-This Atom package allows you to compile and run C++ and C code within the editor.
+This Atom package allows you to compile and run C++ and C within the editor.
 
 To compile C++, press F5 or right click the file in tree view and click `Compile and Run C++`.
 
 To compile C, press F6 or right click the file in tree view and click `Compile and Run C`.
 
-On Linux, g++ and gcc probably come with your distribution. Run `which g++ gcc && echo You have g++ and gcc. || echo You don\'t have g++ and gcc.`.
+## Dependencies
 
-On Windows, you'll need to install [MinGW](http://www.mingw.org/) and [add it to your PATH](http://www.howtogeek.com/118594/how-to-edit-your-system-path-for-easy-command-line-access/).
+This package relies on a C / C++ compiler (gcc).
 
-On a Mac, you'll need to install [XCode](https://developer.apple.com/xcode/).
+### Linux
 
-# Contributing
+The GNU Compiler Collection may come with your distribution. Run `which gcc g++` to find out.
 
-If you would like to contribute, please run npm test (after [installing ESLint](https://www.npmjs.com/package/eslint)), which will eslint index.js before making a pull request and fix any errors it gives you.
+If that command does not output
+
+```
+/usr/bin/gcc
+/usr/bin/g++
+```
+
+you will need to install it.
+
+For RHEL-based distros, run `sudo dnf install gcc gcc-c++`.
+
+For Debian-based distros, run `sudo apt install gcc g++`.
+
+For Arch-based distros, run `sudo pacman -S gcc`.
+
+### Windows
+
+You'll need to install [MinGW](http://www.mingw.org/) and [add it to your PATH](http://www.howtogeek.com/118594/how-to-edit-your-system-path-for-easy-command-line-access/).
+
+### Mac
+
+You'll need to install [XCode](https://developer.apple.com/xcode/).
+
+## Contributing
+
+<!-- i did not test this "guide", so it may not work perfectly -->
+
+```bash
+# to start, you must fork this project
+git clone https://github.com/<your username>/atom-gpp-compiler.git
+cd atom-gpp-compiler
+rm -rf ~/.atom/packages/gpp-compiler
+apm link # this will create a symbolic link from this directory to ~/.atom/packages
+git checkout -b my-changes # this will create a new branch, change `my-changes` to something else
+# make your changes
+npm test # this will eslint `index.js`, if this gives you any errors, fix them
+# test your changes in Atom, because you ran `apm link`, this package will automatically load
+git add .
+git commit -m "added a feature"
+git push
+# go to https://github.com/<your username>/atom-gpp-compiler.git and click `Create Pull Request`
+```
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/* jshint node: true, browser: true */
-/* globals atom */
+/* global atom */
+
 "use strict";
 
 const child_process = require("child_process");
@@ -7,24 +7,30 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = {
-  // function that gets called on package activate
-  activate: () => {
+  activate() {
     // make gpp-compiler:compileGpp (bound to F5) call compileGpp()
-    atom.commands.add("atom-text-editor", "gpp-compiler:compile-gpp", compileGpp);
-    // make clicking "Compile and Run C++" in tree view call treeCompileGpp()
-    atom.commands.add(".tree-view .file", "gpp-compiler:tree-compile-gpp", treeCompileGpp);
-    // make gpp-compiler:compileGcc (bound to F5) call compileGcc()
-    atom.commands.add("atom-text-editor", "gpp-compiler:compile-gcc", compileGcc);
-    // make clicking "Compile and Run C" in tree view call treeCompileGcc()
-    atom.commands.add(".tree-view .file", "gpp-compiler:tree-compile-gcc", treeCompileGcc);
+    atom.
+      commands.
+      add("atom-text-editor", "gpp-compiler:compile-gpp", compileGpp);
+    // make clicking `Compile and Run C++` in tree view call treeCompileGpp()
+    atom.
+      commands.
+      add(".tree-view .file", "gpp-compiler:tree-compile-gpp", treeCompileGpp);
+    // make gpp-compiler:compileGcc (bound to F6) call compileGcc()
+    atom.
+      commands.
+      add("atom-text-editor", "gpp-compiler:compile-gcc", compileGcc);
+    // make clicking `Compile and Run C` in tree view call treeCompileGcc()
+    atom.
+      commands.
+      add(".tree-view .file", "gpp-compiler:tree-compile-gcc", treeCompileGcc);
   },
-  // options
   config: {
     addCompilingErr: {
-      title: "Add compiling_error.txt",
+      title: "Add `compiling_error.txt`",
       type: "boolean",
       default: true,
-      description: "Add a file named \"compiling_error.txt\" if compiling goes wrong"
+      description: "Add a file named 'compiling_error.txt' if compiling goes wrong"
     },
     runAfterCompile: {
       title: "Run After Compile",
@@ -38,53 +44,81 @@ module.exports = {
       default: "",
       description: "Extension of compiled file"
     },
+    // TODO: add separate options for gcc and g++
     gppOptions: {
       title: "gcc/g++ Options",
       type: "string",
       default: "",
-      description: "g++ command line options"
+      description: "gcc/g++ command line options"
     }
   }
 };
 
 // if the user is running linux, add the option to change default terminal
-if (process.platform == "linux") {
-  module.exports.config.linuxTerminal = {
-    title: "Linux terminal",
-    type: "string",
-    default: "XTerm",
-    enum: ["XTerm", "GNOME Terminal", "Konsole", "xfce4-terminal", "pantheon-terminal"]
-  };
+if (process.platform === "linux") {
+  module.
+    exports.
+    config.
+    linuxTerminal = {
+      title: "Linux terminal",
+      type: "string",
+      default: "XTerm",
+      enum: [
+        "XTerm",
+        "GNOME Terminal",
+        "Konsole",
+        "xfce4-terminal",
+        "pantheon-terminal"
+      ]
+    };
 }
 
 // wrapper for compile()
 function compileGpp() {
-  const file = atom.workspace.getActiveTextEditor().buffer.file;
+  const file = atom.
+    workspace.
+    getActiveTextEditor().
+    buffer.
+    file;
+
   if (file) {
     const filePath = file.path;
-    compile("g++", [filePath], path.parse(filePath));
+
+    compile("g++", [
+      filePath
+    ], path.parse(filePath));
   } else {
-    atom.notifications.add("error", "<strong>Compiling error</strong>: File not found.<br/>Save before compiling.");
+    atom.
+      notifications.
+      add("error", "<strong>Compiling error</strong>: File not found.<br/>Save before compiling.");
   }
 }
 
-// wrapper for treeCompile() to compile C++
 function treeCompileGpp(e) {
   treeCompile(e, "g++");
 }
 
 // wrapper for compile() to compile C
 function compileGcc() {
-  const file = atom.workspace.getActiveTextEditor().buffer.file;
+  const file = atom.
+    workspace.
+    getActiveTextEditor().
+    buffer.
+    file;
+
   if (file) {
     const filePath = file.path;
-    compile("gcc", [filePath], path.parse(filePath));
+
+    compile("gcc", [
+      filePath
+    ], path.parse(filePath));
   } else {
-    atom.notifications.add("error", "<strong>Compiling error</strong>: File not found.<br/>Save before compiling.");
+    atom.
+      notifications.
+      add("error", "<strong>Compilation Error</strong>: File not found.<br/>Save before compiling.");
   }
 }
 
-// wrapper for treeCompile() to compile C
 function treeCompileGcc(e) {
   treeCompile(e, "gcc");
 }
@@ -95,17 +129,21 @@ function treeCompile(e, command) {
   const names = document.querySelectorAll(".tree-view .file.selected > .name");
   // array of files to compile
   const files = [];
+
   // file right clicked on
   let element = e.target;
+
   if (element.classList.contains("file")) {
     element = element.firstChild;
   }
+
   // loop through every selected file and push them to files if they are an HTML element
-  for (const i in names) {
-    if (names[i] instanceof HTMLElement) {
-      files.push(names[i].getAttribute("data-path"));
+  for (const file of names) {
+    if (file instanceof HTMLElement) {
+      files.push(file.getAttribute("data-path"));
     }
   }
+
   // call compile, telling it to compile either C++ or C
   compile(command, files, path.parse(element.getAttribute("data-path")));
 }
@@ -113,47 +151,64 @@ function treeCompile(e, command) {
 // spawn gcc or g++ to compile files and possibly run the compiled files
 function compile(command, files, info) {
   // store the current editor in the editor variable
-  const editor = atom.workspace.getActiveTextEditor();
+  const editor = atom.
+    workspace.
+    getActiveTextEditor();
+
   // array of arguments to pass to gcc or g++
-  const args = [];
+  let args = [];
+
   // if the user has an editor open, save it
   if (editor) {
     editor.save();
   }
+
   // set custom output file extension
   if (atom.config.get("gpp-compiler.fileExtension")) {
-    // info.name += "." + atom.config.get("gpp-compiler.fileExtension");
     info.name += `.${atom.config.get("gpp-compiler.fileExtension")}`;
   }
+
   // extend arguments and files
-  // TODO: test if `args = [args, ...files];` works
-  for (const i in files) {
-    args.push(files[i]);
-  }
-  // set output filename
-  args.push("-o");
-  args.push(info.name);
+  args = [
+    ...args,
+    ...files,
+    "-o",
+    info.name
+  ];
+
   // add custom gcc/g++ arguments
-  const userArgs = atom.config.get("gpp-compiler.gppOptions").split(" ");
-  for (const i in userArgs) {
-    if (userArgs[i]) {
-      args.push(userArgs[i]);
+  const userArgs = atom.
+    config.
+    get("gpp-compiler.gppOptions").
+    split(" ");
+
+  for (const arg of userArgs) {
+    if (arg) {
+      args.push(arg);
     }
   }
+
   // spawn gcc/g++ with the working directory of info.dir
   const child = child_process.spawn(command, args, {
     cwd: info.dir
   });
+
   // if the compile exits with a non-zero status, alert the user the error
   let stderr = "";
-  child.stderr.on("data", (data) => {
-    stderr += data;
-  });
+
+  child.
+    stderr.
+    on("data", (data) => {
+      stderr += data;
+    });
   // callback when the child's stdio streams close
   child.on("close", (code) => {
     // if the exit code is a non-zero status, alert the user stderr
     if (code) {
-      atom.notifications.add("error", stderr.replace(/\n/, ""));
+      atom.
+        notifications.
+        add("error", stderr.replace(/\n/, ""));
+
       if (atom.config.get("gpp-compiler.addCompilingErr")) {
         fs.writeFile(path.join(info.dir, "compiling_error.txt"), stderr);
       }
@@ -164,36 +219,63 @@ function compile(command, files, info) {
         const options = {
           cwd: info.dir
         };
-        // if the platform is Windows, run start (which is a shell builtin, so we can't
-        // use child_process.spawn), which spawns a new instance of cmd to run the program
-        if (process.platform == "linux") {
+
+        if (process.platform === "linux") {
           // if the platform is linux, spawn the program in the user set terminal
-          const terminal = atom.config.get("gpp-compiler.linuxTerminal");
+          const terminal = atom.
+            config.
+            get("gpp-compiler.linuxTerminal");
           const file = path.join(info.dir, info.name);
-          if (terminal == "GNOME Terminal") {
-            child_process.spawn("gnome-terminal", ["--command", file], options);
-          } else if (terminal == "Konsole") {
-            child_process.spawn("konsole", ["--hold", "-e", file], options);
-          } else if (terminal == "xfce4-terminal") {
-            child_process.spawn("xfce4-terminal", ["--hold", "--command", file], options);
-          } else if (terminal == "pantheon-terminal") {
-            child_process.spawn("pantheon-terminal", ["-e", file], options);
+
+          if (terminal === "GNOME Terminal") {
+            child_process.spawn("gnome-terminal", [
+              "--command",
+              file
+            ], options);
+          } else if (terminal === "Konsole") {
+            child_process.spawn("konsole", [
+              "--hold",
+              "-e",
+              file
+            ], options);
+          } else if (terminal === "xfce4-terminal") {
+            child_process.spawn("xfce4-terminal", [
+              "--hold",
+              "--command",
+              file
+            ], options);
+          } else if (terminal === "pantheon-terminal") {
+            child_process.spawn("pantheon-terminal", [
+              "-e",
+              file
+            ], options);
           } else {
-            child_process.spawn("xterm", ["-hold", "-e", file], options);
+            child_process.spawn("xterm", [
+              "-hold",
+              "-e",
+              file
+            ], options);
           }
-        } else if (process.platform == "win32") {
+        } else if (process.platform === "win32") {
+          // if the platform is Windows, run start (which is a shell builtin, so we can't
+          // use child_process.spawn), which spawns a new instance of cmd to run the program
           child_process.exec(`start "${info.name}" cmd /C "${path.join(info.dir, info.name)} & echo. & pause"`);
-        } else if (process.platform == "darwin") {
+        } else if (process.platform === "darwin") {
           // if the platform is mac, spawn open, which does the same thing as Windows' start, but
           // is not a builtin, so we can child_process.spawn it
-          child_process.spawn("open", [info.name], options);
+          child_process.spawn("open", [
+            info.name
+          ], options);
         }
       } else {
         // if the user doesn't want the program to run after compilation, give them an alert
         // telling them it was successful
-        atom.notifications.add("success", "Compiling successful");
+        atom.
+          notifications.
+          add("success", "Compiling successful");
       }
-      // since the compilation was successful, remove compiling_error.txt if it exists
+
+      // since the compilation was successful, remove `compiling_error.txt` if it exists
       fs.stat(path.join(info.dir, "compiling_error.txt"), (err) => {
         if (!err) {
           fs.unlink(path.join(info.dir, "compiling_error.txt"));
